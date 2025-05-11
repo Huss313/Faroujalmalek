a47e836a89ac3ba91a12602f19c4dcc6e3c235e4
huss313 <hussrash@gmail.com>
2025-05-11 22:11:43 +0300

    Fix GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Menu from './pages/Menu';
 import Contact from './pages/Contact';
